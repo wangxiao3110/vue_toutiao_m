8ea70e3968ee277845cc66b189305952daa44e28
wangxiao <1029388549@qq.com>
2020-10-27 11:38:31 +0800

    引入图标库，引入全局vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+      <van-button plain type="primary">朴素按钮</van-button>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入vant核心组件库
+import Vant from 'vant'
+// 引入vant组件库样式表
+import 'vant/lib/index.css'
+
 // 引入全局样式表
 import './styles/index.less'
+
+// 使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
