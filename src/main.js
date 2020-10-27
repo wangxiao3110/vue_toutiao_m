e820ca16448889bbe2f0b8601f3ef9e9e2a2c357
wangxiao <1029388549@qq.com>
2020-10-27 15:51:20 +0800

    完成rem适配问题

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import Vant from 'vant'
 // 引入vant组件库样式表
 import 'vant/lib/index.css'
 
+// 全局配置移动端rem适配插件
+// 使用 [lib-flexible]动态设置 REM 基准值（html 标签的字体大小）
+import 'amfe-flexible'
+
 // 引入全局样式表
 import './styles/index.less'
 
